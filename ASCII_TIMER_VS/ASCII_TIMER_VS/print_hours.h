3361cb63de777e64062b78143980f3ee252c2566
Filip Filchev <fkfilchev@gmail.com>
2021-12-24 23:00:42 +0200

    Add some Changes

--- ASCII_TIMER_VS/ASCII_TIMER_VS/print_hours.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#ifndef PRINT_HOURS_H
-#define PRINT_HOURS_H
-void printHours(int hours, int lineNumber, int tensOfNumberCount, bool isHoursLessThanTen);
-#endif
--- ASCII_TIMER_VS/ASCII_TIMER_VS/print_minutes.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#ifndef PRINT_MINUTES_H
-#define PRINT_MINUTES_H
-void printMinutes(const int minutes, const int lineNumber);
-#endif
